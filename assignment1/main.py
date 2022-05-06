7461dabfa193f87b9da97b211c5b8ad4af0e3bc6
Tariq Ganem <tariqg@post.bgu.ac.il>
2022-05-06 15:38:21 +0300

    final version of the code

--- assignment1/main.py
+++ assignment1/main.py
@@ -1,26 +1,28 @@
+import random
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.linear_model import LogisticRegression
+from sklearn.base import is_classifier
 from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
-from sklearn.naive_bayes import GaussianNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier as tc, DecisionTreeClassifier
 from sklearn import tree
 from sklearn.utils.validation import check_is_fitted
-from sklearn.model_selection import KFold
+from sklearn.metrics import balanced_accuracy_score
 
 
 class MyDecisionTreeClassifier(tc):
+    ALPHA_PROB = 0.1
+
     def predict_proba(self, X, check_input=True):
+
         check_is_fitted(self)
         X = self._validate_X_predict(X, check_input)
-        # proba = self.tree_.predict(X)
-        proba = self.myPredict(X)
+        arr = []
+        for i in range(len(X)):
+            arr.append(self.myPredict(X, i))
+        proba = np.array(arr)
         if self.n_outputs_ == 1:
             proba = proba[:, : self.n_classes_]
             normalizer = proba.sum(axis=1)[:, np.newaxis]
@@ -39,84 +41,87 @@ class MyDecisionTreeClassifier(tc):
                 proba_k /= normalizer
                 all_proba.append(proba_k)
 
-            return all_proba
+        return all_proba
 
-    def myPredict(self, X):
+    def myPredict(self, x_test, sample_id):
 
+        children_left = self.tree_.children_left
+        children_right = self.tree_.children_right
         feature = self.tree_.feature
         threshold = self.tree_.threshold
 
-        node_indicator = self.decision_path(X)
-        leaf_id = self.myApply(X)
+        stack = [0]
+        while len(stack) > 0:
+            node_id = stack.pop()
+            if children_left[node_id] == children_right[node_id]:
+                return self.tree_.value[node_id][0]
 
-        sample_id = 0
-        # obtain ids of the nodes `sample_id` goes through, i.e., row `sample_id`
-        node_index = node_indicator.indices[
-                     node_indicator.indptr[sample_id]: node_indicator.indptr[sample_id + 1]
-                     ]
-        print("Rules used to predict sample {id}:\n".format(id=sample_id))
-        for node_id in node_index:
-            # continue to the next node if it is a leaf node
-            if leaf_id[sample_id] == node_id:
-                continue
-
-            # check if value of the split feature for sample 0 is below threshold
-            if X_test[sample_id, feature[node_id]] <= threshold[node_id]:
-                threshold_sign = "<="
+            to_test = float(x_test[sample_id, feature[node_id]])
+            if to_test <= threshold[node_id]:
+                if random.random() >= self.ALPHA_PROB:
+                    stack.append(children_left[node_id])
+                else:
+                    stack.append(children_right[node_id])
             else:
-                threshold_sign = ">"
+                if random.random() >= self.ALPHA_PROB:
+                    stack.append(children_right[node_id])
+                else:
+                    stack.append(children_left[node_id])
 
-            print(
-                "decision node {node} : (X_test[{sample}, {feature}] = {value}) "
-                "{inequality} {threshold})".format(
-                    node=node_id,
-                    sample=sample_id,
-                    feature=feature[node_id],
-                    value=X_test[sample_id, feature[node_id]],
-                    inequality=threshold_sign,
-                    threshold=threshold[node_id],
-                )
-            )
+    def predict(self, X, check_input=True):
+        check_is_fitted(self)
+        X = self._validate_X_predict(X, check_input)
+        arr = []
+        for i in range(len(X)):
+            arr.append(self.myPredict(X, i))
+        proba = arr
+        proba = np.array(proba)
+        n_samples = X.shape[0]
 
-        return None
+        # Classification
+        if is_classifier(self):
+            if self.n_outputs_ == 1:
+                return self.classes_.take(np.argmax(proba, axis=1), axis=0)
 
-    def myApply(self, X):
-        n_nodes = self.tree_.node_count
-        children_left = self.tree_.children_left
-        children_right = self.tree_.children_right
+            else:
+                class_type = self.classes_[0].dtype
+                predictions = np.zeros((n_samples, self.n_outputs_), dtype=class_type)
+                for k in range(self.n_outputs_):
+                    predictions[:, k] = self.classes_[k].take(
+                        np.argmax(proba[:, k], axis=1), axis=0
+                    )
 
-        pass
+                return predictions
+
+        # Regression
+        else:
+            if self.n_outputs_ == 1:
+                return proba[:, 0]
+
+            else:
+                return proba[:, :, 0]
 
 
 if __name__ == '__main__':
     balance_data = pd.read_csv(
-        'C:\\Users\\wiggl\\Desktop\\Networking\\Data_for_UCI_named.csv',
+        'C:\\Users\\wiggl\\Desktop\\Data_for_UCI_named.csv',
         sep=',', header=None)
 
     X = balance_data.values[1:, 0:12]
     Y = balance_data.values[1:, 12]
 
-    # Splitting the dataset into train and test
     X_train, X_test, y_train, y_test = train_test_split(
-        X, Y, test_size=0.2, random_state=7)
+        X, Y, test_size=0.2, random_state=None)
 
-    # clf = MyDecisionTreeClassifier(criterion='entropy', max_depth=3)
-    clf = tc(criterion='entropy', max_depth=3)
-    clf.fit(X_train, y_train)
+    my_clf = MyDecisionTreeClassifier(criterion='entropy', max_depth=3)
 
-    k = clf.predict(X_test)
+    my_clf.fit(X_train, y_train)
 
-    x = clf.predict_proba(X_test)
+    predicts = my_clf.predict_proba(X_test)
 
-    z = accuracy_score(y_test, k)
+    z = accuracy_score(y_test, my_clf.predict(X_test))
 
-    models = []
-    models.append(('KNN', KNeighborsClassifier()))
-    models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
-    models.append(('LDA', LinearDiscriminantAnalysis()))
-    #models.append(('SVM', SVC(gamma='auto')))
-    #models.append(('NB', GaussianNB()))
-    models.append(('CART', DecisionTreeClassifier()))
+    models = [('MODIFIED', MyDecisionTreeClassifier()), ('ORIGINAL', DecisionTreeClassifier())]
     results = []
     names = []
     for name, model in models:
@@ -126,31 +131,31 @@ if __name__ == '__main__':
         msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
         print(msg)
 
-    knn = KNeighborsClassifier()
+    knn = MyDecisionTreeClassifier()
     knn.fit(X_train, y_train)
     predictions = knn.predict(X_test)
     print(accuracy_score(y_test, predictions))
 
-    print(confusion_matrix(y_test, predictions))
+    matrix = confusion_matrix(y_test, predictions)
+    accuracy = (matrix[0][0] + matrix[1][1]) / (matrix[0][0] + matrix[1][1] + matrix[0][1] + matrix[1][0])
+    print(accuracy)
+
+    print(balanced_accuracy_score(y_test, predictions))
+
     print(classification_report(y_test, predictions))
 
-'''
-fig = plt.figure()
+    fig = plt.figure()
     fig.suptitle('Algorithm Comparison')
     ax = fig.add_subplot(111)
     plt.boxplot(results)
     ax.set_xticklabels(names)
     plt.show()
-'''
 
-'''
+    print(my_clf.tree_.feature)
 
-    print(clf.tree_.feature)
+    print(my_clf.tree_.threshold)
 
-    print(clf.tree_.threshold)
-
-    tree.plot_tree(clf, fontsize=10)
+    tree.plot_tree(my_clf, fontsize=10)
     plt.show()
     print(z)
 
-'''
